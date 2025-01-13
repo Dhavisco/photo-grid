1198fa63a1e501253cc1fc80435639ccf3690ec5
Dhavisco <olaniyidavid2002@gmail.com>
2025-01-13 11:25:19 +0100

    refactoring

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -9,23 +9,23 @@ import room from "../assets/bedroom.webp";
 
 const Index = () => {
   return (
-    <div className="m-10 flex flex-col gap-4">
+    <div className="m-4 flex flex-col gap-4">
       <ul className="grid grid-cols-4 gap-4">
-        <li className="col-span-1 border-1 rounded-3xl bg-red-500 h-96">
+        <li className="col-span-1 h-96 lg:h-[34rem]">
           <img
             src={sea}
             alt="sea"
             className="rounded-3xl object-cover w-full h-full aspect-square"
           />
         </li>
-        <li className="col-span-2 border-1 rounded-3xl bg-green-500 h-96">
+        <li className="col-span-2 h-96 lg:h-[34rem]">
           <img
             src={chun}
             alt="sea"
             className="rounded-3xl object-cover w-full h-full aspect-[4/3]"
           />
         </li>
-        <li className="col-span-1 border-1 rounded-3xl bg-blue-500 h-96">
+        <li className="col-span-1 h-96 lg:h-[34rem]">
           <img
             src={vis}
             alt="sea"
@@ -34,15 +34,15 @@ const Index = () => {
         </li>
       </ul>
       <ul className="grid grid-cols-4 gap-4">
-        <li className="flex flex-col gap-2 justify-between col-span-1 border-1 rounded-3xl h-96">
-          <div className="border-1 rounded-3xl bg-pink-700 h-48">
+        <li className="flex flex-col gap-2 justify-between col-span-1 h-96 lg:h-[34rem]">
+          <div className=" h-48 lg:h-[16.5rem]">
             <img
               src={rest}
               alt="sea"
               className="rounded-3xl object-cover w-full h-full aspect-square"
             />
           </div>
-          <div className="border-1 rounded-3xl bg-violet-700 h-48">
+          <div className=" h-48 lg:h-[16.5rem]">
             <img
               src={baddie}
               alt="sea"
@@ -50,22 +50,22 @@ const Index = () => {
             />
           </div>
         </li>
-        <li className="col-span-1 border-1 rounded-3xl bg-gray-700 h-96">
+        <li className="col-span-1  h-96 lg:h-[34rem]">
           <img
             src={astro}
             alt="sea"
             className="rounded-3xl object-cover w-full h-full aspect-square"
           />
         </li>
-        <li className="flex flex-col gap-2 justify-between col-span-2 border-1 rounded-3xl h-96">
-          <div className="border-1 rounded-3xl bg-orange-700 h-48">
+        <li className="flex flex-col gap-2 justify-between col-span-2 h-96 lg:h-[34rem]">
+          <div className=" h-48 lg:h-[16.5rem]">
             <img
               src={room}
               alt="sea"
               className="rounded-3xl object-cover w-full h-full aspect-square"
             />
           </div>
-          <div className="border-1 rounded-3xl bg-red-500 h-48">
+          <div className=" h-48 lg:h-[16.5rem]">
             <img
               src={dark}
               alt="sea"
